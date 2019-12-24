25d22ccf8559805fadf976f4041a773a26c1ecf8
wangliya <13269512462@163.com>
2019-12-24 09:52:17 +0800

    add styles

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'learning_logs',
     'users'
 ]
@@ -121,3 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_URL='/users/login/'
+
+# django-bootstrap3的设置
+BOOTSTRAP3 = { 'include_jquery': True, }
